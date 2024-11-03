d9c24f521d3aa17c10cf60b4ced782cceb7a3544
p <145280448+jsjsjsia@users.noreply.github.com>
2024-11-03 22:06:24 +0700

    hah

--- new.cpp
+++ new.cpp
@@ -3,14 +3,12 @@
 using namespace std;
 int main()
 {
-    freopen("CDIV.INP", "r", stdin);
-    freopen("CDIV.OUT", "w", stdout);
     int n, kq, a; cin>>n;
     int x; cin>> x;
     for(int i=0; i<= n; i++)
     {
        cin>> a;
-       kq = gcd(x,a);
+       cout << x + a;
     }
     cout << kq;
 }
